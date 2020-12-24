90e19e34352b361d4ce75b2fa6478b1a50eaff9c
OFTNgames <pmenesick@gmail.com>
2020-12-23 20:27:45 -0500

    Refactor Pooling 12/23

--- Assets/Scripts/enemyPool.cs
+++ Assets/Scripts/enemyPool.cs
@@ -7,61 +7,46 @@ public class enemyPool : MonoBehaviour
     public static enemyPool enemyPoolInstance;
     public int xPos;
     public int zPos;
-    public int enemyCount;
     public int enemySpawnCap = 4;
     public float spawnRate = 5f;
 
-    [SerializeField]
-    private GameObject pooledEnemy;
+    [SerializeField] private GameObject enemyPrefab;
     private bool notEnoughEnemyInPool = true;
-    private Queue<GameObject> availableObjects = new Queue<GameObject>();
-    public List<GameObject> enemy;
+    public int enemyCount;
+    private List<GameObject> enemies;
 
-    private void Awake()
-    {
-        enemyPoolInstance = this;
-    }
+    private void Awake() => enemyPoolInstance = this;
 
-    // Start is called before the first frame update
     void Start()
     {
-        enemy = new List<GameObject>();
-        
+        enemies = new List<GameObject>();
         StartCoroutine(SpawnCap());
-          
     }
-
     
     IEnumerator SpawnCap()
     {
         while (true)
         {
-            //StartCoroutine(EnemyDrop());
             yield return new WaitForSeconds(spawnRate);
             enemySpawnCap += 1;
-            //Debug.Log(enemySpawnCap);
         }
     }
 
-
-
     void Update()
     {
-
-
-        enemyCount = enemy.Count;
-
+        enemyCount = enemies.Count;
         if (enemyCount < enemySpawnCap)
-            {
+        {
             StartCoroutine(EnemyDrop());
-            }
         }
+    }
+
     IEnumerator EnemyDrop()
     {
         xPos = Random.Range(80, 105);
         zPos = Random.Range(-35, -47);
 
-        GameObject newEnemy = enemyPool.enemyPoolInstance.GetEnemy();
+        GameObject newEnemy = GetEnemy();
         newEnemy.transform.position = new Vector3(xPos, 3, zPos);
         newEnemy.SetActive(true);
 
@@ -69,37 +54,26 @@ public class enemyPool : MonoBehaviour
         enemyCount += 1;
     }
 
-
-
-
     public GameObject GetEnemy()
     {
-        if (enemy.Count > 0)
+        if (enemies.Count > 0)
         {
-            for (int i = 0; i < enemy.Count; i++)
+            for (int i = 0; i < enemies.Count; i++)
             {
-                if (!enemy[i].activeInHierarchy)
+                if (!enemies[i].activeInHierarchy)
                 {
-                   // pooledEnemy = availableObjects.Dequeue();
-                    return enemy[i];
-
+                    return enemies[i];
                 }
             }
         }
 
         if (notEnoughEnemyInPool)
         {
-            GameObject bul = Instantiate(pooledEnemy);
-            bul.SetActive(false);
-           // availableObjects.Enqueue(pooledEnemy);
-            enemy.Add(bul);
-            return bul;
+            GameObject newEnemy = Instantiate(enemyPrefab);
+            newEnemy.SetActive(false);
+            enemies.Add(newEnemy);
+            return newEnemy;
         }
-
         return null;
-
     }
-
-
-   
 }
